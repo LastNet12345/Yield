5f93b56cedda504af73fd1920f22513978757eed
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-14 13:53:48 +0100

    First try

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var numbers = GetNumbers();
+            IEnumerator<int> enumerator = numbers.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                int item = enumerator.Current;
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in GetNumbers())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable<int> GetNumbers()
+        {
+            //return new List<int> { 1, 2, 3, 4, 5 };
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
         }
     }
 }
